e6255b17f1fa4032c13a638c544c8becaaa5fb84
caiof <caiofn.dev@gmail.com>
2026-03-23 17:27:36 -0300

    Modelando prompts

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 import os
 
@@ -9,12 +10,26 @@ numero_dias = 7
 numero_crianças = 2
 atividade = "praia"
 
-prompt = f"Crie um roteiro de viagens para um período de {numero_dias}dias, para uma família com {numero_crianças}, que busca atividades relacionadas a {atividade}"
+modelo_de_prompt = PromptTemplate(
+    template= """
+    Crie um roteiro de viagem de {dias} dias,
+    para uma família com {numero_crianças} crianças,
+    que gostam de {atividade}
+    """
+) # type: ignore
+
+prompt = modelo_de_prompt.format(
+    dias = numero_dias,
+    numero_crianças = numero_crianças,
+    atividade = atividade
+)
+
+print("Prompt : \n", prompt)
 
 modelo = ChatOpenAI(
     model= "gpt-3.5-turbo",
     temperature=0.5,
-    api_key=api_key
+    api_key=api_key # type: ignore
 )
 
 resposta = modelo.invoke(prompt)
